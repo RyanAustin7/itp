8bec7773cb8cad92ad590c48fb438f578ca6b5ad
RyanAustin7 <raustintex@gmail.com>
2024-02-27 16:00:58 -0500

    Added code
    I further made fun commented out code.

--- Mice/3blindMice.py
+++ Mice/3blindMice.py
@@ -1,9 +1,16 @@
-#a = 1 + 1 + 1
-#print(f"{a}" "\n\tblind\n\t\tmice")
-
 #below is the actual Blind Mice Code!
 b = int(input("How many mice are there? "))
 print(f"{b}" "\n\tBlind\n\t\tMice")
 
+## ORIGINAL MICE CODE
+#a = 1 + 1 + 1
+#print(f"{a}" "\n\tblind\n\t\tmice")
+
+## CONVERSATION SIMULATOR
 #k = str(input("What is your name? "))
 #print("Hello, " f"{k}" ". Nice to meet you!")
+
+## TEMPERATURE CONVERTER
+#u = int(input("What is the current temperature outside in farennheit? "))
+#temp = (u-32)*5/9
+#print("That means it is currently",f"{temp:.1f}","degrees celsius outside.")
