1e03ccfd9d449bef452bc304a03f5189d312c57a
RyanAustin7 <raustintex@gmail.com>
2024-02-27 15:58:20 -0500

    Updated Code!
    Added some fun things to the code. They're commented out so it's not going to mess with anything but it's fun.

--- Mice/3blindMice.py
+++ Mice/3blindMice.py
@@ -1,5 +1,9 @@
 #a = 1 + 1 + 1
 #print(f"{a}" "\n\tblind\n\t\tmice")
 
+#below is the actual Blind Mice Code!
 b = int(input("How many mice are there? "))
 print(f"{b}" "\n\tBlind\n\t\tMice")
+
+#k = str(input("What is your name? "))
+#print("Hello, " f"{k}" ". Nice to meet you!")
