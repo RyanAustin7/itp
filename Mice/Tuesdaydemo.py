7fbc2e8ea2f71be765e46a009349d834cea66063
RyanAustin7 <raustintex@gmail.com>
2024-03-11 11:47:31 -0400

    Other things that maybe should be pushed?
    I honestly don't know what these are or if they should be put into my folders but they seem fine to push forward,So that's what I'm gonna do for organization's sake.

--- Mice/Tuesdaydemo.py
+++ Mice/Tuesdaydemo.py
@@ -1,3 +1,12 @@
-i = int(input("What is the current temperature outside in farennheit? "))
-temp = (i-32)*5/9
-print("That means it is currently",f"{temp:.1f}""°C outside.") 
+#i = int(input("What is the current temperature outside in farennheit? "))
+#temp = (i-32)*5/9
+#print("That means it is currently",f"{temp:.1f}""°C outside.") 
+
+
+midiNote = 64
+if midiNote < 64:
+  print("MIDI note is smaller than 64.")
+elif midiNote > 64:
+  print("MIDI note is greater than 64.")
+else:
+  print("MIDI note is equal to 64.")
